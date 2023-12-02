2f1ee922249b405e8332bf2af27084a1f7f4a4f2
Fabio Cruz <fabinhocosta1202@gmail.com>
2023-12-02 18:38:57 -0300

    :hammer: Implement email layout

---
+++ resources/views/emails/includes/head.blade.php
@@ -0,0 +1,5 @@
+<title>{{ config('app.name') }}</title>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<meta content="width=device-width, initial-scale=1.0" name="viewport" />
+<!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+<link href="{{ asset('css/style-email.css') }}" rel="stylesheet">
